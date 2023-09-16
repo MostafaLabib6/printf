3e4be36a6885181d8c662a294354a0fc65135f92
mostafa <mostafa@example.com>
2023-09-16 19:40:19 +0300

    update to betty style.

--- main.h
+++ main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 
-#include <stdarg.h> // used by => va_list, va_start, va_arg, va_end
-#include<string.h> // used by => strlen
-#include <unistd.h> // used by => write
+#include <stdarg.h> /* used by => va_list, va_start, va_arg, va_end */
+#include<string.h> /* used by => strlen */
+#include <unistd.h> /* used by => write */
 
 /* Function to be implemeanted */
 int _printf(const char *format, ...);
 
 
-#endif // MAIN_H
+#endif /* MAIN_H */

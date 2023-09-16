ee4a8509df251842be9655802fcdac8bd22a08f3
mostafa <mostafa@example.com>
2023-09-16 19:25:20 +0300

    add _printf function prototype to header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H  
+#define MAIN_H
+
+
+#include <stdarg.h> // used by => va_list, va_start, va_arg, va_end
+#include<string.h> // used by => strlen
+#include <unistd.h> // used by => write
+
+/* Function to be implemeanted */
+int _printf(const char *format, ...);
+
+
+#endif // MAIN_H

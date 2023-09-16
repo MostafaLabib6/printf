c44bf8b4b9f0f99c3b0d12d469fb05b603dc7de7
mostafa <mostafa@example.com>
2023-09-16 19:55:52 +0300

    task 1 :Implemeant printf functiion with %% %C %S specifiers

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include"main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: an array of chars
+ *
+ *
+ * Return: int printed chars length
+ */
+
+int _printf(const char *format, ...)
+{
+	/* accumlator to keep track of the number of printed characters*/
+	int printed_chars_len = 0;
+
+	va_list args;
+	va_start(args, format);
+
+	/*check for empty input string*/	
+	if (format == NULL)
+		return (-1);
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			++printed_chars_len;
+		}
+		else
+		{
+			++format;
+			if(*format == '%')
+			{
+				write(1, format, 1);
+				++printed_chars_len;
+			}
+			else if(*format == 'c')
+			{
+				char c = va_arg(args, int);
+				/* printf("%c", c); // TODO: remove this line after testing */
+				/* printf("%c", &c); // TODO: remove this line after testing */
+				write(1, &c, 1);
+				++printed_chars_len;
+			}
+			else if(*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				write(1, s, strlen(s));
+				printed_chars_len += strlen(s);
+			}
+		}
+		++format;
+	}
+	/*clear the memory*/
+	va_end(args);
+	return (printed_chars_len);
+
+}

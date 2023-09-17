42e820ee2d0587bd59976074d577386563801ac7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-17 21:40:10 +0300

    add unit tests
    Co-Authored-By: Zaiyyo <135516625+Zaiyyo@users.noreply.github.com>

--- test/main.c
+++ test/main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
  * main - Entry point
@@ -30,6 +30,14 @@ int main(void)
 
 	printf("----------------new checks------\n");
 	_printf("length[len %d , len2 %d]\n", len, len2);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 
 	len = _printf("mostafa % %  ");
 	printf("mostafa % %  ");

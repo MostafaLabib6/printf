91744a0dfeb992bab89e32c92be2e65710e985a0
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-17 20:40:07 +0300

    Update _printf.c
    Co-Authored-By: Zaiyyo <135516625+Zaiyyo@users.noreply.github.com>

--- _printf.c
+++ _printf.c
@@ -10,53 +10,55 @@
 
 int _printf(const char *format, ...)
 {
-	/* accumlator to keep track of the number of printed characters*/
+		fm format_redear[] = {
+							{"%c", print_char}, {"%s", print_string},
+							{NULL, NULL}
+							};
+
+	/* accumlator to keep tracflagof the number of printed characters*/
 	int printed_chars_len = 0;
+	int flag, i = -1, j = 0;
 	/* buffer to store the string representation of an integer using itoa*/
 	/*char tmp_buffer[10000];*/
-
 	va_list args;
-	va_start(args, format);
-
 	/*check for empty input string*/
 	if (format == NULL)
 		return (-1);
-
-	while (*format != '\0')
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[++i])
 	{
-		if (*format != '%')
+		j = 0, flag = 0;
+		if (format[i] == '%') /* % yd */
 		{
-			write(1, format, 1);
-			++printed_chars_len;
+			if (format[i + 1] == 32 || format[i + 1] == 9) /*check for tab or space */
+				i++, printed_chars_len++;
+			if (format[i + 1] == '%') /* printf "%%" */
+			{
+				i += 1, printed_chars_len += _putchar(format[i]);
+				continue;
+			}
+			if (format[i + 1] == '\0') /*"%  %" */
+			{
+				printed_chars_len = _putchar(format[i]);
+				return (printed_chars_len);
+			}
+			while (format_redear[j].f) /* %   yd*/
+			{
+				if (format[i + 1] == format_redear[j].f[1])
+				{
+					printed_chars_len += format_redear[j].print(args); i++; flag = 1;
+					break;
+				}
+				else
+					j++;
+			}
+			if (!flag)
+				printed_chars_len += _putchar(format[i]);
 		}
 		else
-		{
-			++format;
-			if (*format == '\0')
-				return (-1);
-
-			else if (*format == '%')
-			{
-				write(1, format, 1);
-				++printed_chars_len;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				++printed_chars_len;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				write(1, s, strlen(s));
-				printed_chars_len += strlen(s);
-			}
-			else
-				return (-1);
-
-		}
-		++format;
+			printed_chars_len += _putchar(format[i]);
 	}
 	/*clear the memory*/
 	va_end(args);

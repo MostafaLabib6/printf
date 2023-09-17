06480abd787d006c50f1d027519c647206648a88
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-17 20:58:38 +0300

    Update _printf.c
    Co-Authored-By: Zaiyyo <135516625+Zaiyyo@users.noreply.github.com>

--- _printf.c
+++ _printf.c
@@ -10,57 +10,24 @@
 
 int _printf(const char *format, ...)
 {
-		fm format_redear[] = {
-							{"%c", print_char}, {"%s", print_string},
-							{NULL, NULL}
-							};
+	fm format_redear[] = {
+						{"%c", print_char}, {"%s", print_string},
+						{NULL, NULL}
+						};
 
-	/* accumlator to keep tracflagof the number of printed characters*/
-	int printed_chars_len = 0;
-	int flag, i = -1, j = 0;
-	/* buffer to store the string representation of an integer using itoa*/
-	/*char tmp_buffer[10000];*/
 	va_list args;
+	int printed_chars_len = 0;
+
 	/*check for empty input string*/
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
+
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (format[++i])
-	{
-		j = 0, flag = 0;
-		if (format[i] == '%') /* % yd */
-		{
-			if (format[i + 1] == 32 || format[i + 1] == 9) /*check for tab or space */
-				i++, printed_chars_len++;
-			if (format[i + 1] == '%') /* printf "%%" */
-			{
-				i += 1, printed_chars_len += _putchar(format[i]);
-				continue;
-			}
-			if (format[i + 1] == '\0') /*"%  %" */
-			{
-				printed_chars_len = _putchar(format[i]);
-				return (printed_chars_len);
-			}
-			while (format_redear[j].f) /* %   yd*/
-			{
-				if (format[i + 1] == format_redear[j].f[1])
-				{
-					printed_chars_len += format_redear[j].print(args); i++; flag = 1;
-					break;
-				}
-				else
-					j++;
-			}
-			if (!flag)
-				printed_chars_len += _putchar(format[i]);
-		}
-		else
-			printed_chars_len += _putchar(format[i]);
-	}
-	/*clear the memory*/
-	va_end(args);
+
+	printed_chars_len = get_print(format, args, format_redear);
 	return (printed_chars_len);
+
 }

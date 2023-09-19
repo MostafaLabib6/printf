8ca945c0765301470ce708e402f75bc57645b1d5
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-19 14:26:06 +0300

    Update main.h
    Co-Authored-By: Zaiyyo <135516625+Zaiyyo@users.noreply.github.com>

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <stdarg.h> /* used by => va_list, va_start, va_arg, va_end */
 #include<string.h> /* used by => strlen */
 #include <unistd.h> /* used by => write */
-
+#include <stdlib.h>
 
 /**
  * struct formats - Struct formats
@@ -30,6 +30,7 @@ int _printf(const char *format, ...);
 /*_write file*/
 int _putchar(char c);
 int _puts(char *s);
+void write_base(char *str);
 /*print_char*/
 int print_char(va_list arg);
 /*print_string*/
@@ -40,4 +41,8 @@ int get_print(const char *format, va_list arg, struct formats *format_redear);
 int print_integer(va_list list);
 int print_number(va_list args);
 int print_binary(va_list args);
+/*helper_function*/
+unsigned int base_len(unsigned int num, int base);
+char *rev_string(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /* MAIN_H */
